c93db74d1b0170b985bb5eb90fdc677139926123
David Sanchez <davix.sevas@gmail.com>
2023-07-13 17:14:31 -0500

    Se realiza funcion para calcular el perimetro y area, tambien se realiza configuracion de las etiquetas META en el archivo HTML.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//Funciones cuadrado
 function PerimetroCuadrado(){
     var ladosCuadrado = document.getElementById("ladoCuadrado");
     var valor = ladosCuadrado.value;
@@ -15,3 +17,27 @@ function AreaCuadrado(){
     var setresult = document.getElementById("result");
     setresult.textContent = "El área del cuadrado es: "+valor+"cm";
 }
+
+//Funciones triangulo
+function PerimetroTriangulo(){
+    var ladoUnoTriangulo = document.getElementById("ladoUnoTriangulo");
+    var ladoDosTriangulo = document.getElementById("ladoDosTriangulo");
+    var ladoTresTriangulo = document.getElementById("ladoTresTriangulo");
+
+    var valorUno = parseInt(ladoUnoTriangulo.value);
+    var valorDos = parseInt(ladoDosTriangulo.value);
+    var valorTres = parseInt(ladoTresTriangulo.value);
+
+    if(resultado = null){
+        resultado = 0;
+    }else{
+        var resultado = valorUno+valorDos+valorTres;
+    }
+    
+   
+    var setresult = document.getElementById("result");
+    setresult.textContent = "El perímetro del triángulo es: "+resultado+"cm";
+}
+
+
+//Funciones circulo.
