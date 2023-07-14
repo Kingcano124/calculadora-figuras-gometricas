9763424732b08c33b919db2e0caff25c4fbd021a
David Sánchez Cano <davix.sevas@gmail.com>
2023-07-13 21:26:12 -0500

    Se realizan las ultimas modficaciones del proyecto.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,32 @@
 //Funciones cuadrado
 function PerimetroCuadrado(){
     var ladosCuadrado = document.getElementById("ladoCuadrado");
-    var valor = ladosCuadrado.value;
-    valor = valor*4;
+    var valor = parseInt(ladosCuadrado.value.trim());
 
-    var setresult = document.getElementById("result");
-    setresult.textContent = "El perímetro del cuadrado es: "+valor+"cm";
+    if (isNaN(valor)) {
+        alert("El campo es obligatorio. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
+
+        valor = valor*4;
+
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El perímetro del cuadrado es: "+valor+"cm";
+    }
 }
 
 function AreaCuadrado(){
     var ladosCuadrado = document.getElementById("ladoCuadrado");
-    var valor = ladosCuadrado.value;
-    valor = valor*valor;
-
-    var setresult = document.getElementById("result");
-    setresult.textContent = "El área del cuadrado es: "+valor+"cm";
+    var valor = parseInt(ladosCuadrado.value.trim());
+   
+    if (isNaN(valor)) {
+        alert("El campo es obligatorio. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
+        valor = valor*valor;
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El área del cuadrado es: "+valor+"cm";
+    }
 }
 
 //Funciones triangulo
@@ -24,20 +36,92 @@ function PerimetroTriangulo(){
     var ladoDosTriangulo = document.getElementById("ladoDosTriangulo");
     var ladoTresTriangulo = document.getElementById("ladoTresTriangulo");
 
-    var valorUno = parseInt(ladoUnoTriangulo.value);
-    var valorDos = parseInt(ladoDosTriangulo.value);
-    var valorTres = parseInt(ladoTresTriangulo.value);
+    var valorUno = parseInt(ladoUnoTriangulo.value.trim());
+    var valorDos = parseInt(ladoDosTriangulo.value.trim());
+    var valorTres = parseInt(ladoTresTriangulo.value.trim());
 
-    if(resultado = null){
-        resultado = 0;
-    }else{
-        var resultado = valorUno+valorDos+valorTres;
-    }
-    
+    if (isNaN(valorUno) || isNaN(valorDos) || isNaN(valorTres)) {
+        alert("Los campos son obligatorios. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
    
-    var setresult = document.getElementById("result");
-    setresult.textContent = "El perímetro del triángulo es: "+resultado+"cm";
+        var resultado = valorUno+valorDos+valorTres;
+
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El perímetro del triángulo es: "+resultado+"cm";
+    }
 }
 
 
+function areaTriangulo(){
+    var base = document.getElementById("BaseTriangulo");
+    var ladoTriangulo = document.getElementById("ladoUTriangulo");
+   
+    var valorUno = parseInt(base.value.trim());
+    var valorDos = parseInt(ladoTriangulo.value.trim());
+
+    if (isNaN(valorUno) || isNaN(valorDos)) {
+        alert("Los campos son obligatorios. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
+   
+        var resultado = (valorUno*valorDos)/2;
+
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El área del triángulo es: "+resultado+"cm";
+    }
+}
+
 //Funciones circulo.
+
+function perimetroCirculo(){
+    let pi =  Math.PI;
+    var radio = document.getElementById("radio");
+    var valor =   parseInt(radio.value.trim());
+
+    if (isNaN(valor)) {
+        alert("El campo es obligatorio. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
+    
+        var resultado = 2*pi*valor;
+        resultado = resultado.toFixed(2);
+
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El perímetro del circulo es: "+resultado+"cm";
+    }
+}
+
+function areaCirculo(){
+    let pi =  Math.PI;
+    var radio = document.getElementById("radio");
+    var valor =   parseInt(radio.value.trim());
+
+    if (isNaN(valor)) {
+        alert("El campo es obligatorio. Por favor, ingrese un valor numérico.");
+        return false;
+    } else {
+    
+        var resultado = pi*(valor*valor);
+        resultado = resultado.toFixed(2);
+
+        var setresult = document.getElementById("result");
+        setresult.textContent = "El área del circulo es: "+resultado+"cm";
+    }
+}
+
+function diametroCirculo() {
+    var radio = document.getElementById("radio");
+    var valor = parseInt(radio.value.trim());
+  
+    if (isNaN(valor)) {
+      alert("El campo es obligatorio. Por favor, ingrese un valor numérico.");
+      return false;
+    } else {
+      var resultado = 2 * valor;
+  
+      var setresult = document.getElementById("result");
+      setresult.textContent = "El diámetro del círculo es: " + resultado + " cm";
+    }
+}
+  
